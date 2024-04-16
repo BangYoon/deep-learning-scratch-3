6728a3adcb6412782e3cb4cb9bac02ed7f06001b
BangYoon <yoonb720@naver.com>
2024-04-16 20:32:49 +0900

    __init__

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,9 +1,17 @@
-from dezero.core_simple import Variable
-from dezero.core_simple import Function
-from dezero.core_simple import using_config
-from dezero.core_simple import no_grad
-from dezero.core_simple import as_array
-from dezero.core_simple import as_variable
-from dezero.core_simple import setup_variable
+
+if_simple_core = True
+
+if if_simple_core:
+    from dezero.core_simple import Variable
+    from dezero.core_simple import Function
+    from dezero.core_simple import using_config
+    from dezero.core_simple import no_grad
+    from dezero.core_simple import as_array
+    from dezero.core_simple import as_variable
+    from dezero.core_simple import setup_variable
+# else:
+#     from dezero.core import Variable
+#     from dezero.core import Function
+
 
 setup_variable()
