d20d3f8443fba23d2fe6a74f6cc58f2591816c97
BangYoon <yoonb720@naver.com>
2024-04-16 19:38:03 +0900

    Clean Code

--- steps/step21.py
+++ steps/step21.py
@@ -2,9 +2,28 @@
 # 21.1 as_variable
 # 21.2 float, int
 # 21.3 __rmul__, __radd__
+# 21.4 __array_priority__
 
 import weakref
 import numpy as np
+import contextlib
+
+
+class Config:
+    enable_backprop = True
+
+@contextlib.contextmanager
+def using_config(name, value):  # 18.5 mode transmition
+  old_value = getattr(Config, name)
+  setattr(Config, name, value)
+  try:
+    yield
+  finally:
+    setattr(Config, name, old_value)
+
+def no_grad():
+  return using_config('enable_backprop', 'False')
+
 
 class Variable:
     __array_priority__ = 200
@@ -13,44 +32,41 @@ class Variable:
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} type is not allowed.'.format(type(data)))
+
         self.data = data
         self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
 
+    @property  # shape method 를 인스턴스 변수처럼 사용할 수 있음
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
     def __len__(self):
       return len(self.data)
 
-    # 객체를 문자열로 representation
     def __repr__(self):
       if self.data is None:
         return 'variable(None)'
-      p = str(self.data).replace('\n', '\n' + ' '*9)
+      p = str(self.data).replace('\n', '\n' + ' ' * 9)
       return 'variable(' + p + ')'
 
     def set_creator(self, func):
         self.creator = func
         self.generation = func.generation + 1   # parent func's gen + 1
-
-    # def __mul__(self, other):
-    #   return mul(self, other)
-
-    @property # shape method 를 인스턴스 변수처럼 사용할 수 있음
-    def shape(self):
-      return self.data.shape
-
-    @property
-    def ndim(self):
-      return self.data.ndim
-
-    @property
-    def size(self):
-      return self.data.size
-
-    @property
-    def dtype(self):
-      return self.data.dtype
 
     def cleargrad(self):
         self.grad = None
@@ -59,14 +75,15 @@ class Variable:
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
-        # 16.3 Add add_func() in Variable's backward()
         funcs = []
         seen_set = set()
+
         def add_func(f):
             if f not in seen_set:
                 funcs.append(f)
                 seen_set.add(f)
                 funcs.sort(key=lambda x:x.generation)
+
         add_func(self.creator)
 
         while funcs:
@@ -81,6 +98,7 @@ class Variable:
                     x.grad = gx
                 else:
                     x.grad = x.grad + gx
+
                 if x.creator is not None:
                     add_func(x.creator)
 
@@ -88,35 +106,9 @@ class Variable:
                 for y in f.outputs:
                     y().grad = None     # y is weakref
 
-class Function:
-    def __call__(self, *inputs):
-        inputs = [as_variable(x) for x in inputs]
-        xs = [x.data for x in inputs]
-        ys = self.forward(*xs)
-        if not isinstance(ys, tuple):    # if not tuple, make tuple
-            ys = (ys,)
-
-        outputs = [Variable(as_array(y)) for y in ys]
-        if Config.enable_backprop:
-            self.generation = max([x.generation for x in inputs])   # 1. 역전파 모드에서만 세대 필요함
-            for output in outputs:
-                output.set_creator(self)    # 2. 계산들의 연결도 역전파 모드에서만 필요함
-            self.inputs = inputs    # inputs cnt=1
-            self.outputs = [weakref.ref(output) for output in outputs]
-        return outputs if len(outputs) > 1 else outputs[0]
-
-    def forward(self, x):
-        raise NotImplementedError()
-
-    def backward(self, gy):
-        raise NotImplementedError()
-
-class Config:
-    enable_backprop = True  # 역전파 활성 모드
-
 
 def as_array(y):
-    if np.isscalar(y):  # == if not isinstance(y, np.ndarray):
+    if np.isscalar(y):
         return np.array(y)
     return y
 
@@ -126,10 +118,39 @@ def as_variable(obj):
     return Variable(obj)
 
 
+class Function:
+    def __call__(self, *inputs):
+        inputs = [as_variable(x) for x in inputs]
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):    # if not tuple, make tuple
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])   # 1. 역전파 모드에서만 세대 필요함
+            for output in outputs:
+                output.set_creator(self)    # 2. 계산들의 연결도 역전파 모드에서만 필요함
+            self.inputs = inputs            # inputs cnt=1
+            self.outputs = [weakref.ref(output) for output in outputs]
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        return x0 + x1
+    def backward(self, gy):
+        return gy, gy
+
 class Mul(Function):
   def forward(self, x0, x1):
     return x0 * x1
-
   def backward(self, gy):
     x0, x1 = self.inputs[0].data, self.inputs[1].data
     return gy * x1, gy * x0
@@ -137,68 +158,38 @@ class Mul(Function):
 class Square(Function):
     def forward(self, x):
         return x ** 2
-
     def backward(self, gy):
-        x = self.inputs[0].data     # change to inputs
+        x = self.inputs[0].data
         return 2 * x * gy
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
-
     def backward(self, gy):
-        x = self.inputs[0].data     # change to inputs
+        x = self.inputs[0].data
         return np.exp(x) * gy
 
-def mul(x0, x1):
-    x1 = as_array(x1)
-    return Mul()(x0, x1)
-
-def square(x): # simplifying method 1
-    return Square()(x)
-
-def exp(x):
-    return Exp()(x)
-
-x = Variable(np.array(0.5))
-y = square(exp(square(x)))
-y.backward()
-print(x.grad)
-
-class Add(Function):
-    def forward(self, x0, x1):
-        y = x0 + x1
-        return y
-
-    def backward(self, gy):
-        return gy, gy       # partial derivative -> TOOD : fix Variable backward()
 
 def add(x0, x1):
     x1 = as_array(x1)
     return Add()(x0, x1)
 
+def mul(x0, x1):
+    x1 = as_array(x1)
+    return Mul()(x0, x1)
 
-x0 = Variable(np.array(2.0))
-x1 = Variable(np.array(3.0))
-ys = add(x0, x1)
-ys.backward()
-print(ys.data, x0.grad, x1.grad)
+def square(x):
+    return Square()(x)
 
-z = add(square(x0), square(x1))
-z.backward()
-print(z.data)
-print(x0.grad)  # 2x0
-print(x1.grad)  # 2x1
+def exp(x):
+    return Exp()(x)
 
-x = Variable(np.array(3.0))
-y = add(x,x)
-y.backward()
-print(x.data, y.data, x.grad)
 
-x.cleargrad()
-y = add(add(x,x), x)
-y.backward()
-print(x.grad)
+Variable.__add__ = add
+Variable.__radd__ = add
+Variable.__mul__ = mul
+Variable.__rmul__ = mul # 첫 번째 인수가 float/int 인 경우
+
 
 generations = [2,0,1,4,2]
 funcs = []
@@ -207,12 +198,11 @@ for g in generations:
     f.generation = g
     funcs.append(f)
 
-print([f.generation for f in funcs])
+print([f.generation for f in funcs], end=' -> ')
 funcs.sort(key=lambda x:x.generation)
 print([f.generation for f in funcs])
-
 f = funcs.pop()
-print(f.generation)
+print(f.generation) # 4
 
 x = Variable(np.array(2.0))
 a = square(x)
@@ -221,89 +211,18 @@ y.backward()
 print(y.data)
 print(x.grad) # (2x^4)' = 8x^3
 
-
 # 18.1 delete needless gradients
 x0 = Variable(np.array(1.0))
 x1 = Variable(np.array(1.0))
 t = add(x0, x1)
 y = add(x0, t)
 y.backward()
-
 print(y.grad, t.grad)   # needless
 print(x0.grad, x1.grad) # only need
-
-
-# 18.5 mode transmition using "with"
-import contextlib
-@contextlib.contextmanager
-def using_config(name, value):
-  old_value = getattr(Config, name)  # 전처리
-  setattr(Config, name, value)
-  try:
-    yield     # with 블록 들어갈 때 전처리 실행, 블록 나올 때 후처리 실행됨
-  finally:
-    setattr(Config, name, old_value)
-
-"""
-with using_config('enable_backprop','False'):
-  # 이 안에서만 역전파 비활성 모드 - 순전파만 실행함  
-  x = Variable(np.array(2.0))
-  y = square(x)
-"""
-
-def no_grad(): # with using_config 매번 쓰기 귀찮으니까!!
-  return using_config('enable_backprop', 'False')
 
 with no_grad():
   x = Variable(np.array(2.0))
   y = square(x)
-
-
-# 19.
-x = Variable(np.array(([[1,2,3],[4,5,6]])))
-print(x.shape) #shape() 대신 shape
-print(len(x))
-print(x)
-
-
-# 20.
-a = Variable(np.array(3.0))
-b = Variable(np.array(2.0))
-c = Variable(np.array(1.0))
-
-y = add(mul(a,b), c)
-y.backward()
-print(y)
-print(a.grad, b.grad) # 2.0 3.0
-print(c.grad) # 1.0
-print(y.grad) # None
-
-
-# 20. operator overload 다른 방식
-Variable.__mul__ = mul
-Variable.__add__ = add
-
-a = Variable(np.array(3.0))
-b = Variable(np.array(2.0))
-c = Variable(np.array(1.0))
-y = a * b + c
-y.backward()
-print(y)
-print(a.grad, b.grad)
-
-# 21.1
-x = Variable(np.array(2.0))
-y = x + np.array(3.0)
-print(y)
-
-# 21.3 첫 번째 인수가 float or int 인 경우
-#y = 2.0 * x     # typeerror : x 오른쪽에 있으므로 __rmul__ 필요
-Variable.__radd__ = add
-Variable.__rmul__ = mul
-
-x = Variable(np.array(2.0))
-y = 3.0 * x + 1.0
-print(y)
 
 # 21.4 좌항이 ndarray 인스턴스인 경우
 x = Variable(np.array([1.0]))
