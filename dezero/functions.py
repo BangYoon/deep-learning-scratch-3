64c598b40370b9293d2c7860f55a2a9b21afac90
BangYoon <yoonb720@naver.com>
2024-05-13 19:58:53 +0900

    step40 - broadcast_to, sum_to

--- dezero/functions.py
+++ dezero/functions.py
@@ -2,7 +2,7 @@ import numpy as np
 from dezero.core import Function
 from dezero.core import as_variable
 from dezero import utils
-
+from dezero.utils import sum_to
 
 class Sin(Function):
     def forward(self, x):
@@ -84,3 +84,37 @@ class Sum(Function):
 
 def sum(x, axis=None, keepdims=False):
     return Sum(axis, keepdims)(x)
+
+class BroadcastTo(Function):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = np.broadcast_to(x, self.shape)
+        return y
+    def backward(self, gy):
+        gx = sum_to(gy, self.x_shape)
+        return gx
+
+def broadcast_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return BroadcastTo(shape)(x)
+
+class SumTo(Function):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = utils.sum_to(x, self.shape)
+        return y
+    def backward(self, gy):
+        gx = broadcast_to(gy, self.x_shape)
+        return gx
+
+def sum_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return SumTo(shape)(x)
