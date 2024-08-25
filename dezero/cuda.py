d32bde4fd4abfb4dfa62284aac8b54969192608d
BangYoon <yoonb720@naver.com>
2024-08-25 15:54:06 +0900

    Add cuda func using CuPy

--- dezero/cuda.py
+++ dezero/cuda.py
@@ -25,3 +25,23 @@ def get_array_module(x):
         return np
     xp = cp.get_array_module(x)
     return xp
+
+
+def as_numpy(x):
+    if isinstance(x, Variable):
+        x = x.data
+
+    if np.isscalar(x):
+        return np.array(x)
+    elif isinstance(x, np.ndarray):
+        return x
+    return cp.asnumpy(x)
+
+def as_cupy(x):
+    if isinstance(x, Variable):
+        x = x.data
+
+    if not gpu_enable:
+        raise Exception('Cannot load CuPy. You need to install CuPy!')
+    return cp.asarray(x)
+
